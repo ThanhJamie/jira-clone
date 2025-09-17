a123b3795873710f12b5a1a3ac15fa3eae6b92ef
ThanhJamie <thanhjamieai@gmail.com>
2025-09-17 15:15:54 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,147 @@
+import CompanyCarousel from "@/components/company-carousel";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ArrowRight,
+  BarChart,
+  Calendar,
+  ChevronRight,
+  Layout,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import faqs from "../data/faqs.json";
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workfloIs it accessible?w and optimize team productivity with our easy-to-use Kanban boards.",
+    icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    icon: BarChart,
+  },
+];
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <section className="container mx-auto py-20 text-center">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
+          Streamline Your Workflow <br />
+          <span className="flex mx-auto gap-3 sm:gap-4 items-center">
+            with{" "}
+            <Image
+              src={"/logo2.png"}
+              alt="Zscum Logo"
+              width={400}
+              height={80}
+              className="h-14 sm:h-24 w-auto object-contain"
+            />
+          </span>
+        </h1>
+        <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
+          Empower your team with our intuitive project management solution
+        </p>
+        <Link href="/onboarding">
+          <Button size={"lg"} className={"mr-2"}>
+            Get Started <ChevronRight size={18} className="ml-1" />
+          </Button>
+        </Link>
+        <Link href="#features">
+          <Button size={"lg"} variant={"outline"} className={"mr-2"}>
+            Get Started <ChevronRight size={18} className="ml-1" />
+          </Button>
+        </Link>
+      </section>
+      {/* Features */}
+      <section id="features" className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Key Features</h3>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => (
+              <Card key={index}>
+                <CardContent>
+                  <feature.icon className="h-12 w-12 mb-4 text-blue-400" />
+                  <h4 className="text-xl font-semibold mb-2">
+                    {feature.title}
+                  </h4>
+                  <p className="text-gray-300">{feature.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+      {/* Carousel */}
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">
+            Trusted by Teams Worldwide
+          </h3>
+          <CompanyCarousel />
+        </div>
+      </section>
+      {/* FAQ */}
+      <section className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">
+            Frequently Asked Questions
+          </h3>
+          <Accordion>
+            {faqs.map((faq, index) => (
+              <AccordionItem
+                value={`item-${index}`}
+                key={index}
+                className={"w-full"}
+              >
+                <AccordionTrigger>{faq.question}</AccordionTrigger>
+                <AccordionContent>{faq.answer}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
+      </section>
+      {/* CTA */}
+      <section className="py-20 text-center">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-6 text-center">
+            Ready to Transform Your Workflow?
+          </h3>
+          <p className="text-xl mb-12">
+            Join thousands of teams already using ZCRUM to streamline their
+            projects and boost productivity.
+          </p>
+          <Link href={"/onboarding"}>
+            <Button size={"lg"} className={"animate-bounce"}>
+              Start For Free <ArrowRight className="ml-2 h-5 w-5" />
+            </Button>
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+}
